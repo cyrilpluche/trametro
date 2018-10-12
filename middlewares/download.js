779486d33db651e1160692569a9a25517e8ab773
PLUCHE Cyril <cyril.pluche@etu.umontpellier.fr>
2018-10-12 17:51:10 +0200

    Download TAM file OK

--- middlewares/download.js
+++ middlewares/download.js
@@ -1,10 +1,16 @@
+var download = require('download-file')
 const getCSV = require('get-csv');
 
 var url = "http://data.montpellier3m.fr/sites/default/files/ressources/TAM_MMM_TpsReel.csv"
 
+var options = {
+    directory: "./",
+    filename: "schedules.csv"
+}
+
 module.exports = {
     fromURL(req, res, next) {
-        getCSV(url)
+        /*getCSV(url)
             .then(rows => {
                 req.body.file = rows[1]
                 next()
@@ -16,6 +22,28 @@ module.exports = {
                     status: 500
                 }
                 next(error)
-            })
+            });*/
+        try {
+            download(url, options, e => {
+                if (e) {
+                    error = {
+                        type: 'INTERNAL_ERROR',
+                        message: "Error is : " + e,
+                        status: 500
+                    }
+                    next(error)
+                }
+                req.body.file = "Ok dude."
+                next()
+            });
+        } catch (e) {
+            error = {
+                type: 'INTERNAL_ERROR',
+                message: "Error is : " + e,
+                status: 500
+            }
+            next(error)
+        }
+
     }
 }
