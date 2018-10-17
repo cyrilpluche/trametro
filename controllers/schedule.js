99271c459f5b767547114b7280f91a44889d0c22
PLUCHE Cyril <cyril.pluche@etu.umontpellier.fr>
2018-10-17 16:22:06 +0200

    Fix all schedules output

--- controllers/schedule.js
+++ controllers/schedule.js
@@ -13,9 +13,9 @@ module.exports = {
         let answer
         if (req.body.result[0] === "proche") {
             if (req.body.result[1] !== "proche") {
-                answer = 'Le prochain tramway , est ' + req.body.result[0] + '. Les suivants arrivent dans ' + req.body.result[1] + ' et ' + req.body.result[0] + ' minutes à ' + req.param('stop')
+                answer = 'Le prochain tramway , est ' + req.body.result[0] + '. Les suivants arrivent dans ' + req.body.result[1] + ' et ' + req.body.result[2] + ' minutes à ' + req.param('stop')
             } else {
-                answer = 'Les deux prochains tramway , sont ' + req.body.result[0] + '. Le suivant arrive dans ' + req.body.result[0] + ' minutes à ' + req.param('stop')
+                answer = 'Les deux prochains tramway , sont ' + req.body.result[0] + '. Le suivant arrive dans ' + req.body.result[2] + ' minutes à ' + req.param('stop')
             }
         } else {
             answer = 'Les prochains tramway arrivent dans ' + req.body.result[0] + ', ' + req.body.result[1] + ' et ' + req.body.result[2] + ' minutes à ' + req.param('stop')
