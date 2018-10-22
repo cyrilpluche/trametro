b8836c009ed83948a28b61a335b617033f507659
PLUCHE Cyril <cyril.pluche@etu.umontpellier.fr>
2018-10-22 12:02:11 +0200

    Find One From Db v4

--- routes/schedule.js
+++ routes/schedule.js
@@ -11,9 +11,9 @@ var filterMiddleware = require('../middlewares').filter;
 router.get('/find_from_web', downloadMiddleware.fromURL, scheduleController.findOne);
 
 // http://localhost:3000/api/schedule/find_one?id=1&stop=antigone&destination=odysseum&isFromDb=false
-router.get('/find_one', downloadMiddleware.fromURL, tripController.findOne, parseMiddleware.parse, filterMiddleware.filtre, parseMiddleware.intToString, scheduleController.findOne);
+router.get('/find_one', downloadMiddleware.fromURL, parseMiddleware.parse, filterMiddleware.filtre, parseMiddleware.intToString, scheduleController.findOne);
 // http://localhost:3000/api/schedule/find_one_from_db?id=1&session=sessionId&isfav=falseOrTrue&isFromDb=true
-router.get('/find_one_from_db', downloadMiddleware.fromURL, parseMiddleware.parse, filterMiddleware.filtre, parseMiddleware.intToString, scheduleController.findOne);
+router.get('/find_one_from_db', downloadMiddleware.fromURL, tripController.findOne, parseMiddleware.parse, filterMiddleware.filtre, parseMiddleware.intToString, scheduleController.findOne);
 router.get('/find_all', downloadMiddleware.fromURL, parseMiddleware.parse, filterMiddleware.filtre, parseMiddleware.intToString, scheduleController.findAll);
 
 
