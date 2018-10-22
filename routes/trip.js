b6e4576d7cdb24950e65cc5caae4cd8cf413a20d
PLUCHE Cyril <cyril.pluche@etu.umontpellier.fr>
2018-10-22 10:59:33 +0200

    Find one trip fix v1

--- routes/trip.js
+++ routes/trip.js
@@ -2,10 +2,11 @@ var express = require('express');
 var router = express.Router();
 var tripController = require('../controllers').trip;
 
-//router.get('/find_one', tripController.findOne)
+router.get('/find_one', tripController.findOne)
 //router.post('/create', tripController.create);
 
 // localhost:3000/api/trip/find_one?id=alexaid&session=sessionid&isfav=true
+// find the favorite/not favorite trip. Then create it if needed. Then update it.
 router.post('/update', tripController.findOne, tripController.create, tripController.update);
 
 module.exports = router;
