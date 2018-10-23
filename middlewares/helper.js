95a6b9617da14db0083cb714f49759781c8c2c5c
PLUCHE Cyril <cyril.pluche@etu.umontpellier.fr>
2018-10-23 19:47:39 +0200

    Stable version 2.0 - Helper for all suspicious names OK

--- middlewares/helper.js
+++ middlewares/helper.js
@@ -15,9 +15,22 @@ module.exports = {
             else if (station === "gare saint roch") return "gare st-roch"
             else if (station === "hopital lapeyronie") return "hop. lapeyronie"
             else if (station === "place de l'europe") return "pl. de l'europe"
+            else if (station === "rives du lez") return "rives du lez t1"
+            else if (station === "mosson") return "mosson t1"
             else return station
         } else if (ligne === "2") {
-
+            if (station === "corum") return "corum t2"
+            else if (station === "gare st-roch") return "gare st-roch t2"
+            else return station
+        } else if (ligne === "3") {
+            if (station === "gare st-roch") return "gare st-roch t3"
+            else if (station === "rives du lez") return "rives du lez t3"
+            else if (station === "mosson") return "mosson t3"
+            else return station
+        } else if (ligne === "4") {
+            if (station === "gare st-roch") return "gare st-roch t3"
+            if (station === "place albert 1er") return "albert 1er"
+            else return station
         } else {
             return station
         }
