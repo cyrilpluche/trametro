9a6e7885ea64e0f3371f5bafedc2dd850ac10827
PLUCHE Cyril <cyril.pluche@etu.umontpellier.fr>
2018-10-23 19:49:55 +0200

    Stable version 2.0 - undefined schedule OK

--- controllers/schedule.js
+++ controllers/schedule.js
@@ -1,25 +1,31 @@
 module.exports = {
     findOne(req, res) {
-
-        if (req.body.result[0] === "proche") {
-            answer = 'Le tramway ligne ' + req.body.ligneTAM + ', direction ' + req.body.directionTAM + ', est ' + req.body.result[0] + ' de ' + req.body.stationTAM
-        } else {
-            answer = 'Le tramway ligne ' + req.body.ligneTAM + ', direction ' + req.body.directionTAM + ', arrive dans ' + req.body.result[0] + ' minutes à ' + req.body.stationTAM
+        let answer
+        if (req.body.result[0]) {
+            if (req.body.result[0] === "proche") {
+                answer = 'Le tramway ligne ' + req.body.ligneTAM + ', direction ' + req.body.directionTAM + ', est ' + req.body.result[0] + ' de ' + req.body.stationTAM
+            } else {
+                answer = 'Le tramway ligne ' + req.body.ligneTAM + ', direction ' + req.body.directionTAM + ', arrive dans ' + req.body.result[0] + ' minutes à ' + req.body.stationTAM
+            }
         }
-        return res.status(201).json(answer)
+        else answer = "Horaires indisponibles pour le moment."
+        return res.status(201).send(answer)
     },
 
     findAll(req, res) {
         let answer
-        if (req.body.result[0] === "proche") {
-            if (req.body.result[1] !== "proche") {
-                answer = 'Le prochain tramway , est ' + req.body.result[0] + '. Les suivants arrivent dans ' + req.body.result[1] + ' et ' + req.body.result[2] + ' minutes à ' + req.param('stop')
+        if (req.body.result[0]) {
+            if (req.body.result[0] === "proche") {
+                if (req.body.result[1] !== "proche") {
+                    answer = 'Le prochain tramway , est ' + req.body.result[0] + '. Les suivants arrivent dans ' + req.body.result[1] + ' et ' + req.body.result[2] + ' minutes à ' + req.param('stop')
+                } else {
+                    answer = 'Les deux prochains tramway , sont ' + req.body.result[0] + '. Le suivant arrive dans ' + req.body.result[2] + ' minutes à ' + req.param('stop')
+                }
             } else {
-                answer = 'Les deux prochains tramway , sont ' + req.body.result[0] + '. Le suivant arrive dans ' + req.body.result[2] + ' minutes à ' + req.param('stop')
+                answer = 'Les prochains tramway arrivent dans ' + req.body.result[0] + ', ' + req.body.result[1] + ' et ' + req.body.result[2] + ' minutes à ' + req.param('stop')
             }
-        } else {
-            answer = 'Les prochains tramway arrivent dans ' + req.body.result[0] + ', ' + req.body.result[1] + ' et ' + req.body.result[2] + ' minutes à ' + req.param('stop')
         }
-        return res.status(201).json(answer)
+        else answer = "Horaires indisponibles pour le moment."
+        return res.status(201).send(answer)
     }
 }
